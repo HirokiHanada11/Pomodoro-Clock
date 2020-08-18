d3456ed9c2c4a59645f0df0dd126f9f5e10e9f15
Hiroki <hiroki11hanada@gmail.com>
2020-08-18 17:47:08 +0900

    Updates

---
+++ precache-manifest.ad4775cb956f4bc5210a66124bf4056a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452b6166394056aed168f3e8d3373566",
+    "url": "/Pomodoro-Clock/index.html"
+  },
+  {
+    "revision": "6ed6f3f2665be4ecccd8",
+    "url": "/Pomodoro-Clock/static/css/main.144a1854.chunk.css"
+  },
+  {
+    "revision": "0076ac0a65b93a94ef84",
+    "url": "/Pomodoro-Clock/static/js/2.72122bcc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro-Clock/static/js/2.72122bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed6f3f2665be4ecccd8",
+    "url": "/Pomodoro-Clock/static/js/main.b577f9e1.chunk.js"
+  },
+  {
+    "revision": "4ccfd468b853680e7d1a",
+    "url": "/Pomodoro-Clock/static/js/runtime-main.46c75fa3.js"
+  }
+]);
